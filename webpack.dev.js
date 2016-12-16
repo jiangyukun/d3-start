8d95fa4d357f43a0943b97bacc33f1e1260c494b
jiangyukun <187_6810_5877@sina.com>
2016-12-16 17:40:49 +0800

    init commit

---
+++ webpack.dev.js
@@ -0,0 +1,37 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        './js/src/index.js'
+    ],
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+        port: 3000
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:3000/static/'
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"dev"'
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [
+            {test: /\.js$/, loaders: ['babel'], exclude: /node_modules/, include: __dirname},
+            {test: /\.less$/, loader: 'style!css!autoprefixer!less'},
+            {test: /\.scss$/, loader: 'style!css!autoprefixer!sass?sourceMap'},
+            {test: /\.(jpg|png)$/, loader: "url?limit=8192"}
+        ]
+    }
+}
